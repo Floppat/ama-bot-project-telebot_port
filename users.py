917d367c4fae2bdb7f80c3a0cf828d0b7748ad21
who? <164739638+Floppat@users.noreply.github.com>
2025-06-05 16:21:39 +0300

    Update users.py

--- users.py
+++ users.py
@@ -5,6 +5,7 @@ from pets import Enemy, Pet
 from functional import db
 
 
+
 class User:
     def __init__(self, data: tuple):
         self.user_id = data[0]
@@ -12,14 +13,15 @@ class User:
         if None in db.read('users',data[0],'pet_id'):
             db.new_pet(data[0],'дружок',100,100,100,100,1,2,0,5,1,1,0,0)
         self.pet = Pet(db.read('pets', db.read('users',data[0],'pet_id')[0], '*'))
-        self.username = data[2]
-        self.nickname = data[3]
-        self.status_PK = data[4]
-        self.status = db.read('status_keys', data[4], 'status')[0]
-        self.coins = data[5]
-        self.quiz_record = data[6]
-        self.register_date = data[7]
-        self.xp = data[8]
+        self.tag = data[2]
+        self.username = data[3]
+        self.nickname = data[4]
+        self.status_PK = data[5]
+        self.status = db.read('status_keys', data[5], 'status')[0]
+        self.coins = data[6]
+        self.quiz_record = data[7]
+        self.register_date = data[8]
+        self.xp = data[9]
         self.right_answers = 0
 
 
@@ -35,30 +37,30 @@ class User:
 
     def train(self, message, bot):
         if not self.pet.can_train():
-            bot.send_message(message.chat.id, 'сперва вашему питомцу следует восстановить силы')
+            bot.send_message(message.chat.id, 'Сперва вашему питомцу следует восстановить силы.')
             return
         self.pet.train()
         db.change('pets',self.pet_PK,'hp',self.pet.hp)
         db.change('pets',self.pet_PK,'sp',self.pet.sp)
         db.change('pets',self.pet_PK,'str',self.pet.str)
         db.change('pets',self.pet_PK,'xp',self.pet.xp)
-        bot.send_message(message.chat.id, f'питомец прошёл изнурительные тренировки\n{self.pet}')
+        bot.send_message(message.chat.id, f'Питомец прошёл изнурительные тренировки:\n{self.pet}')
         return
 
 
     def feed(self, message, bot):
         if not self.pet.can_feed():
-            bot.send_message(message.chat.id, 'Ваш питомец не голоден')
+            bot.send_message(message.chat.id, 'Ваш питомец не голоден.')
             return
         self.pet.feed()
         db.change('pets',self.pet_PK,'hp',self.pet.hp)
-        bot.send_message(message.chat.id, f'питомец сытно поел\n{self.pet}')
+        bot.send_message(message.chat.id, f'Питомец сытно поел:\n{self.pet}')
         return
 
 
     def attack(self, message, bot):
         if not self.pet.can_attack():
-            bot.send_message(message.chat.id, 'сперва вашему питомцу следует восстановить силы')
+            bot.send_message(message.chat.id, 'Сперва вашему питомцу следует восстановить силы.')
             return
         enemy = Enemy(target_pet=self.pet)
         now_enemy=deepcopy(enemy)
@@ -68,8 +70,8 @@ class User:
             self.pet.attack(target_pet=enemy)
 
             if not self.pet:
-                bot.send_message(message.chat.id, f'вы проиграли!, ваш враг был:{now_enemy}\n'
-                                                        f'ваши характеристики:\n{self.pet}')
+                bot.send_message(message.chat.id, f'Вы проиграли! Ваш враг был:{now_enemy}\n'
+                                                  f'Ваши характеристики:\n{self.pet}')
                 break
 
             if not enemy:
@@ -77,10 +79,10 @@ class User:
                 self.pet.max_str += 2
                 self.pet.avg += 2
                 self.pet.min_def += 1
-                bot.send_message(message.chat.id, f'вы выиграли. противник стал сильнее. ваш враг был:{now_enemy}\n'
-                                'вы заработали 100 монет\n'
-                                f'итого монет: {self.coins}\n'
-                                f'ваши характеристики:\n{self.pet}')
+                bot.send_message(message.chat.id, f'Вы выиграли. Противник стал сильнее. Ваш враг был:{now_enemy}\n'
+                                'Вы заработали 100 монет.\n'
+                                f'Итого монет: {self.coins}\n'
+                                f'Ваши характеристики:\n{self.pet}')
                 break
         db.change('pets',self.pet_PK,'hp',self.pet.hp)
         db.change('pets',self.pet_PK,'sp',self.pet.sp)
@@ -93,11 +95,11 @@ class User:
 
     def sleep(self, message, bot):
         if not self.pet.can_sleep():
-            bot.send_message(message.chat.id, 'Ваш питомец ещё не устал')
+            bot.send_message(message.chat.id, 'Ваш питомец ещё не устал.')
             return
         self.pet.sleep()
         db.change('pets',self.pet_PK,'sp',self.pet.sp)
-        bot.send_message(message.chat.id, f'питомец выспался\n{self.pet}')
+        bot.send_message(message.chat.id, f'Питомец выспался:\n{self.pet}')
         return
 
 
@@ -112,16 +114,16 @@ class User:
         message_items = '\n'.join([f'{item=}; {shop_pets[item].shop()}' for item in shop_pets])
 
         if item not in shop_pets or item in ('?', 'help', 'items'):
-            bot.send_message(message.chat.id, f'Важно! При покупке артефактов сбрасывается опыт и сила (так как для питомца подобное снаряжение в новинку)\n'
-                                                    f'Также, враги слабее не станут. игра рассчитана на убить время когда вам скучно, не торопитесь\n'
+            bot.send_message(message.chat.id, f'Важно! При покупке артефактов сбрасывается опыт и сила (так как для питомца подобное снаряжение в новинку).\n'
+                                                    f'Также, враги слабее не станут. Игра рассчитана на убить время когда вам скучно, не торопитесь.\n'
                                                     f'Доступные для покупки артефакты:\n{message_items}')
             return
 
         if self.coins < shop_pets[item].price:
-            bot.send_message(message.chat.id, f'Недостаточно монет: чтобы купить этот артефакт, нужно {shop_pets[item].price} монет')
+            bot.send_message(message.chat.id, f'Недостаточно монет: чтобы купить этот артефакт, нужно {shop_pets[item].price} монет.')
             return
         elif self.pet.xp < shop_pets[item].xp_price:
-            bot.send_message(message.chat.id, f'Недостаточно опыта: чтобы купить этот артефакт, нужно {shop_pets[item].xp_price} опыта')
+            bot.send_message(message.chat.id, f'Недостаточно опыта: чтобы купить этот артефакт, нужно {shop_pets[item].xp_price} опыта.')
             return
         
         self.coins -= shop_pets[item].price
@@ -147,7 +149,7 @@ class User:
         if self.right_answers > self.quiz_record:
             self.quiz_record = self.right_answers
         bot.send_message(message.chat.id, content=f'Вы набрали {self.right_answers}/5 очков{job}\n'
-                                                        f'рекорд:{self.quiz_record}/5 очков.')
+                                                  f'Рекорд:{self.quiz_record}/5 очков.')
         db.change('users',self.user_id,'quiz_record',self.quiz_record)
         self.right_answers = 0
 
@@ -156,13 +158,13 @@ def registred(message):
     try:
         user = db.read('users',message.from_user.id,'*')
     except IndexError:
-        db.new_user(message.from_user.id,'NULL',message.from_user.username,'NULL',1,0,0,str(datetime.datetime.now()).split()[0],0)
-        user = db.read('users',message.from_user.id,'*')
+        db.new_user(message.from_user.id,'NULL',message.from_user.username, message.from_user.full_name, 'игрок со странным именем',1,0,0,str(datetime.datetime.now()).split()[0],0)
+        user = db.read('users',message.from_user.id,'*') 
     return User(user)
 
 def get_other_user(user_tag: str):
     try:
-        other_user = db.read('users',db.get_PK('users','tag',user_tag),'*')
+        other_user = db.read('users',db.get_PK('users','tag',user_tag),'*') 
         return User(other_user)
     except IndexError:
-        return 'указан неверный тег или такой пользователь не пользовался ботом'
+        return 'Указан неверный тег или такой пользователь не пользовался ботом.'
