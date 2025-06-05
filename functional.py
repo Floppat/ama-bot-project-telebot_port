efbc93da4fc434025c289decbd853a4360b1cfb3
who? <164739638+Floppat@users.noreply.github.com>
2025-06-05 16:24:44 +0300

    Update functional.py

--- functional.py
+++ functional.py
@@ -9,18 +9,24 @@ db.create_tables()
 def plus_xp(message):
     db.change('users',message.from_user.id,'xp',db.read('users',message.from_user.id,'xp')[0]+1)
 
-def change_status(message, status_id: int, user_tag: int):
+def change_status(message, status_id: int, user_tag: str):
     if int(db.read('users',message.from_user.id,'status_id')[0]) == 2:
-        return db.change('users',db.get_PK('users','tag',user_tag),'status_id',status_id)
+        try:
+            return db.change('users',db.get_PK('users','tag',user_tag),'status_id',status_id)
+        except IndexError:
+            return 'Указан неверный тег или такой пользователь не пользовался ботом.'
     else:
-        return 'недостаточно прав чтобы выполнить'
+        return 'Недостаточно прав чтобы выполнить.'
     
 def delete_user(message, user_tag: int):
     if int(db.read('users',message.from_user.id,'status_id')[0]) == 2:
-        return db.delete('users',db.get_PK('users','tag',user_tag))
+        try:
+            return db.delete('users',db.get_PK('users','tag',user_tag))
+        except IndexError:
+            return 'Указан неверный тег или такой пользователь не пользовался ботом.'
     else:
-        return 'недостаточно прав чтобы выполнить'
+        return 'Недостаточно прав чтобы выполнить.'
 
 
-async def fetch_args(command: str, source: str) -> list[str]:
+def fetch_args(command: str, source: str) -> list[str]:
     return source.removeprefix(command).strip().split()
